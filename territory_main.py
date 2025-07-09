0333b31d1224645eb05d4e036f290a8537e44862
Sachit <admin@sachitdeshmukh.com>
2025-07-09 23:13:38 +0530

    Add keyboard input to main
    1. Define new function to only allow [Y/N] input from keyboard
    2. Add user-input to SIM_DONE and GEN_GRAPH

--- territory_main.py
+++ territory_main.py
@@ -1,4 +1,5 @@
 import logging  # Logging setup for monitoring execution
+import msvcrt # To work with keyboard input
 
 # Set logging format and level
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -7,9 +8,20 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 import territory_simulate
 import territory_graphs
 
+def ask_yes_no_keypress(prompt):
+    print(prompt, end='', flush=True)
+    while True:
+        key = msvcrt.getch().decode('utf-8').lower()
+        if key == 'y':
+            print('Y')
+            return True
+        elif key == 'n':
+            print('N')
+            return False
+
 def main():
-    SIM_DONE = True # user to define before each simulation
-    GEN_GRAPH = True # user to define before each simulation
+    SIM_DONE = ask_yes_no_keypress("Is the simulation already complete? [Y/N]: ") # user to define before each simulation
+    GEN_GRAPH = ask_yes_no_keypress("Do you want to generate the graphs? [Y/N]: ") # user to define before each simulation
 
     while GEN_GRAPH:
         if not SIM_DONE:
